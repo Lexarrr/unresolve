8b6edaf7a8921eb05f49c3115d720119466c1430
Lexarrr <etc.21@yandex.ru>
2023-09-15 19:21:49 +0300

    ??

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.lang.Math.abs;
+
+
 public class Main {
     public static void main(String[] args) throws IOException {
 
@@ -11,27 +14,28 @@ public class Main {
         Scanner sc = new Scanner(new File(inputFile));
         Writer wr = new FileWriter(outputFile);
 
-        List<Character> validFirstChars = Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h');
+//        List<Character> validFirstChars = Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h');
 
-        while (sc.hasNextLine()) {
+        while (sc.hasNextLine() && sc != null) {
             String s = sc.nextLine();
             String[] a = s.split("");
             System.out.println(Arrays.toString(a));
-            if (a.length != 5 || a[2].equals("-")) {
+            if (a.length != 5) {  // || a[2].equals("-")
                 wr.write("Error\n");
                 System.out.println("Error");
                 break;
             }
             int b = Integer.parseInt(a[1]);
             int c = Integer.parseInt(a[4]);
-            if (b - c != 2 && a[0].equals(a[3])) {
+            int d = a[0].compareTo(a[3]);
+            System.out.println(d);
+//            if () {
+//                System.out.println("No");
+//            }
+            if (b - c != 2 || d != -1 || a[0].equals(a[3])) { //??
                 wr.write("No\n");
                 System.out.println("No");
-            }
-            if (){
-                System.out.println("No");
-            }
-            else {
+            } else {
                 wr.write("Yes\n");
                 System.out.println("Yes");
             }
